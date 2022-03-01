bd3a0c3fe0c6ed620acf76e3bbc5df94e9414a2a
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-03-01 23:03:35 +0600

    All done

--- js/script.js
+++ js/script.js
@@ -1,37 +1,65 @@
 // Loard Search Result
-const loadSearchResult = () => {
+const loadSearchResult = (condition=true) => {
+	displayOrHideElement('spinnner-below-search', 'block');
+	displayOrHideElement('search-not-found', 'none');
+	displayOrHideElement('phone-details', 'none');
+	displayOrHideElement('load-more-button', 'none');
+	if(condition===true){
+		document.getElementById('result-card').textContent = ``;
+	} else{
+		displayOrHideElement('spinnner-below-load-more', 'block');
+	}
+
 	const searchText = document.getElementById('search-field').value;
 	const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
 	fetch(url)
 	.then(response => response.json())
-	.then(data => displaySearchResult(data.data));
+	.then(data => displaySearchResult(data.data, condition));
 };
 
 // Display Search Result
-const displaySearchResult = (data) => {
+const displaySearchResult = (data, condition) => {
 	const container = document.getElementById('result-card');
-	document.getElementById('search-not-found').style.display = 'none';
-	document.getElementById('phone-details').style.display = 'none';
+	
+	// Spinner hide after load result
+	displayOrHideElement('spinnner-below-search', 'none');
+	displayOrHideElement('spinnner-below-load-more', 'none');
 
+	// No search found
 	if(data.length === 0){
-		document.getElementById('search-not-found').style.display = 'block';
+		displayOrHideElement('search-not-found', 'block');
 	}
 
 	container.textContent = ``;
-	data.forEach(d => {
+	
+	const lenghtOfResult = data.length;
+
+	//Load More Button Show and Hide Codition
+	if(lenghtOfResult>20 && condition === true){
+		displayOrHideElement('load-more-button', 'block');
+	} 
+
+	let flag = 0;
+	data.every( d => {
+		if(flag===20 && condition === true){
+			return false;
+		}
 		const div = document.createElement('div');
 		div.classList.add('col');
 		div.innerHTML = `
 		<div class="card h-100">
 			<img src="${d.image}" class="card-img-top w-25 mx-auto my-4" alt="">
-			<h4 class="mx-auto mt-4 fs-4">${d.phone_name}</h4>
-			<h5 class="mx-auto mb-4 fs-6">Brand: ${d.brand}</h5	>
+			<h4 class="mx-auto mt-4 fs-4 text-center">${d.phone_name}</h4>
+			<h5 class="mx-auto mb-4 fs-6 text-center">Brand: ${d.brand}</h5	>
 			<button class="btn btn-dark w-50  mx-auto mb-4" onclick="loadPhoneDetails('${d.slug}')">Explore</button>
 	  	</div>
 		`;
 		container.appendChild(div);
+		flag++;
 
+		return true;
 	});
+	
 };
 
 
@@ -48,7 +76,10 @@ const displayPhoneDetails = (data) => {
 	const container = document.getElementById('phone-details');
 	container.textContent = ``;
 	
-	document.getElementById('phone-details').style.display = 'block';
+	displayOrHideElement('phone-details','block');
+
+	//Explore button clicked then auto scroll top
+	window.scrollTo({ top: 0, behavior: 'smooth' });
 	
 	const div = document.createElement('div');
 	div.classList.add('card');
@@ -70,22 +101,15 @@ const displayPhoneDetails = (data) => {
 			  	</h2>
 			  	<div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
 					<div class="accordion-body">
-					  <li><strong>Display Size: </strong>${data.mainFeatures.displaySize}</li>
-					  <li><strong>Chipset: </strong>${data.mainFeatures.chipSet}</li>
-					  <li><strong>Memory: </strong>${data.mainFeatures.memory}</li>
-					  <li><strong>Storage: </strong>${data.mainFeatures.storage}</li>
-					  <li><strong>Sensors: </strong>${data.mainFeatures.sensors}</li>
+					  <li><strong>Display Size: </strong>${data?.mainFeatures?.displaySize ? data.mainFeatures.displaySize : notExistMessage()}</li>
+					  <li><strong>Chipset: </strong>${data?.mainFeatures?.chipSet ? data.mainFeatures.chipSet : notExistMessage()}</li>
+					  <li><strong>Memory: </strong>${data?.mainFeatures?.memory ? data.mainFeatures.memory : notExistMessage()}</li>
+					  <li><strong>Storage: </strong>${data?.mainFeatures?.storage ? data.mainFeatures.storage : notExistMessage()}</li>
+					  <li><strong>Sensors: </strong>${data?.mainFeatures?.sensors ? data.mainFeatures.sensors : notExistMessage()}</li>
 					</div>
 			  	</div>
 			</div>
-	  </div>
-	`;
-	
-	// Others properties checking
-	if(data.others !== undefined){
-		div.innerHTML += `
-		<div class="accordion w-75 mx-auto" id="accordionPanelsStayOpenExample">
-				<div class="accordion-item">
+			<div class="accordion-item">
 				  	<h2 class="accordion-header" id="panelsStayOpen-headingTwo">
 						<button class="accordion-button bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
 						  Others
@@ -93,17 +117,26 @@ const displayPhoneDetails = (data) => {
 				  	</h2>
 				  	<div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingTwo">
 						<div class="accordion-body">
-						<li><strong>WLAN: </strong>${data.others.WLAN}</li>
-						<li><strong>Bluetooth: </strong>${data.others.Bluetooth}</li>
-						<li><strong>GPS: </strong>${data.others.GPS}</li>
-						<li><strong>NFC: </strong>${data.others.NFC}</li>
-						<li><strong>Radio: </strong>${data.others.Radio}</li>
-						<li><strong>USB: </strong>${data.others.USB}</li>
+						<li><strong>WLAN: </strong>${data?.others?.WLAN ? data.others.WLAN : notExistMessage()}</li>
+						<li><strong>Bluetooth: </strong>${data?.others?.Bluetooth ? data.others.Bluetooth: notExistMessage()}</li>
+						<li><strong>GPS: </strong>${data?.others?.GPS ? data.others.GPS : notExistMessage()}</li>
+						<li><strong>NFC: </strong>${data?.others?.NFC ? data.others.NFC : notExistMessage()}</li>
+						<li><strong>Radio: </strong>${data?.others?.Radio ? data.others.Radio : notExistMessage()}</li>
+						<li><strong>USB: </strong>${data?.others?.USB ? data.others.USB : notExistMessage()}</li>
 						</div>
 				  	</div>
 				</div>
-			</div>
-		`;
-	}
+	  </div>
+	`;
 	container.appendChild(div);
+}
+
+// No Exist Message Arrow Function
+const notExistMessage = () => {
+	return "This features doesn't exist";
+}
+
+//Display Or Hide Element Arrow Function
+const displayOrHideElement = (id, displayType) => {
+	document.getElementById(id).style.display = displayType;
 }
